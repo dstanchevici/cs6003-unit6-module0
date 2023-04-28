6b6194aead2961f8d15cd3d03824f6bba21306f1
dstanchevici <distanchev@gmail.com>
2023-04-28 12:11:56 -0400

    0.6

---
+++ TestCase.java
@@ -0,0 +1,19 @@
+public class TestCase {
+
+    public static void main (String[] argv)
+    {
+        int k = 3;
+        switch (k) {
+            case 0:
+                System.out.println("k=" + k);
+            case 1:
+                System.out.println("k=" + k);
+            case 2:
+                System.out.println("k=" + k);
+            case 3:
+                System.out.println("k=" + k);
+            default:
+                System.out.println("k is out of range");
+        }
+    }
+}
